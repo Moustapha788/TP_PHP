8245739e4603db341de4b28506fa83b86492955b
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-02-10 10:39:29 +0000

    first commit exo 13

--- exercices/exercice-13/fonctions.php
+++ exercices/exercice-13/fonctions.php
@@ -1,4 +1,57 @@
 <?php
 
+/* Écrire un programme qui permet de remplir N phrases à partir d’un
+champ TextArea.Le programme enlève tous les espaces inutiles de chaque
+phrase puis ré-affiche les phrases corrigées dans un autre TextArea.
+Les TextArea des phrases corrigées sont à lecture seule.
+
+Règles de Gestion
+● Le champ TextArea de saisie des phrases est Obligatoire
+● Les phrases ne doivent pas contenir des caractères spéciaux
+● Une phrase commence par lettre majuscules et se termine par un point
+● Chaque phrase contiendra au moins 25 caractères */
+
+
+
+
+
+// $phrase='';
+
+/* une fonction sui remarque une fin de phrase en l'occurence un point valide (et non un decimal (1.3)) */
+
+// function lePremierPointRencontré(s)
+
+
+// fonction qui dit si une phrase contient 25 caractères ou non
+function auMoins25Caracteres($phrase):bool{
+    $espaceVide=substr_count($phrase," ");
+    $nbrCar=strlen($phrase)-$espaceVide;
+    return $nbrCar>=25;
+} 
+/* fonction qui corrige chaque phrase qui lui donnée en paramètre en enlevant 
+toutes les majuscules qui  et en mettant le premier caractère en majuscules */  
+function corrigeLaPhrase(string &$phrase):string{
+    $phrase=ucfirst(strtolower($phrase));
+    return $phrase;
+}
+// fonction qui compte le nombre qui enlève les espaces
+function enlèveLesEspacesEntreLesMots(string &$paragraphe):string{
+    $lesMotsDuParagragphe=str_word_count($paragraphe,1,null);
+    $nbrMots=count($lesMotsDuParagragphe);
+    for($i=0;$i<$nbrMots;$i++){
+        htmlspecialchars($lesMotsDuParagragphe[$i]);
+        trim($lesMotsDuParagragphe[$i]);
+    }
+    $paragraphe=implode(" ",$lesMotsDuParagragphe);
+    // supprime l'espace au début du paragraphe
+    ltrim($paragraphe);
+    // retouren moi le pagraphe
+    return $paragraphe;
+} 
+// fonction qui en 
+
+
+
+
 
 ?>
--- exercices/exercice-13/index.php
+++ exercices/exercice-13/index.php
@@ -23,6 +23,46 @@ include("/opt/lampp/htdocs/MES_PROJETS/TP_PHP/fichiers-accueil-html-css-php/menu
 
 
 
+<form action="controller.php" method="POST">
+    <fieldset>
+        <legend>Généreration  de N champs avec html </legend>
+        <div class="form-groups">
+            <!-- champ de saisie -->
+            <div class="champ_de_saisie">
+                <label for="value_of_N" class="labels">Entrez les N phrases puis nous la corrigeons.Une phrase compte au minimum 25 caractères.</label>
+                <textarea  name="value_of_N" id="value_of_N"  class="champ_texte"  placeholder="J'apprends le codage."></textarea>
+            </div>
+            <!-- bontou d'envoie -->
+            <div class="form-validation">
+                <button type="submit" name="validated" class="valider">Correction</button>
+            </div>
+        </div>
+    </fieldset>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php 
 /*  défilement*/
 include("/opt/lampp/htdocs/MES_PROJETS/TP_PHP/fichiers-accueil-html-css-php/defilement.footer.html.php");
